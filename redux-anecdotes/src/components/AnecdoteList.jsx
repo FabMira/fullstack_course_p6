3983b0edabc9ecdf76d160f5341c7d83c0582610
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-06-11 18:38:50 -0400

    paso 13 finalizado (6.13)

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
+import { setNotification, clearNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
@@ -8,6 +9,10 @@ const AnecdoteList = () => {
 
   const handleVote = (id) => {
     dispatch(vote(id))
+    dispatch(setNotification(`you voted '${anecdotes.find(a => a.id === id).content}'`))
+    setTimeout(() => {
+      dispatch(clearNotification())
+    }, 5000)
   }
 
   return (
